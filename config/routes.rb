81be6dd41b6e2312d301f7c3c55c59184e4e5186
Samantha Morco <samanthamorco@gmail.com>
2017-06-11 15:09:00 -0400

    dry up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/employees", to: 'employees#index'
-  get "/employees/:id", to: 'employees#show'
-  post "/employees", to: 'employees#create'
-  patch "/employees/:id", to: 'employees#update'
-  delete "/employees/:id", to: 'employees#destroy'
-
+  # get "/employees", to: 'employees#index'
+  # get "/employees/:id", to: 'employees#show'
+  # post "/employees", to: 'employees#create'
+  # patch "/employees/:id", to: 'employees#update'
+  # delete "/employees/:id", to: 'employees#destroy'
+  # resources :employees, except: [:new, :edit]
+  # resources :employees, only: [:index, :show, :create, :update, :destroy]
   namespace :api do
     namespace :v1 do
-      get "/employees", to: 'employees#index'
-      get "/employees/:id", to: 'employees#show'
-      post "/employees", to: 'employees#create'
-      patch "/employees/:id", to: 'employees#update'
-      delete "/employees/:id", to: 'employees#destroy'
+      resources :employees, except: [:new, :edit]
     end
 
     namespace :v2 do
-      get "/employees", to: 'employees#index'
-      get "/employees/:id", to: 'employees#show'
-      post "/employees", to: 'employees#create'
-      patch "/employees/:id", to: 'employees#update'
-      delete "/employees/:id", to: 'employees#destroy'
+      resources :employees, except: [:new, :edit]
     end
   end
 end

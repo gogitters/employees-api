8b16db87e26992563009f269a88844dceeb99fe6
Samantha Morco <samanthamorco@gmail.com>
2017-06-13 19:39:49 -0400

    poro and secure api

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,6 @@
 class Employee < ApplicationRecord
   has_many :addresses
-  validates :first_name, :last_name, presence: true
+  # validates :first_name, :last_name, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
